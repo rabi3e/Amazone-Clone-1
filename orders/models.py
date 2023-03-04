bb46d7cb6c6842a9cb2e6437729ba5ef8be5ee28
rabi3e <65637491+rabi3e@users.noreply.github.com>
2023-03-04 22:57:34 +0100

    creat order Models

--- orders/models.py
+++ orders/models.py
@@ -1,23 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from product.models import Product
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 ORDER_STATUS = (
-    ('R', 'Recieved'),
-    ('C', 'Processed'),
-    ('E', 'Shipped'),
-    ('E', 'En cour'),
-    
-
+    ('R', 'Reçue'),
+    ('T', 'Traitée'),
+    ('E', 'Expédiée'),
+    ('L', 'Livrée'),
 )
 
 
 class Order(models.Model):
-    pass
+    user= models.ForeignKey("User", verbose_name=_("Client"), on_delete=models.SET_NULL,null=True,blank=True)
+    code = models.CharField(_("Code"), max_length=50)
+    status = models.CharField(_("Status"), max_length=1, choices=ORDER_STATUS)
+    order_time = models.DateTimeField(_("Heure de Commande"), auto_now=True)
+    delivery_time = models.DateTimeField(_("Heure d'expédition"), auto_now=False, auto_now_add=False,null=True,blank=True)
 
 
     def __str__(self):
-        pass
+        self.code
 
     class Meta:
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
+    
+
+class OrderDetail(models.Model):
+    ordre= models.ForeignKey("Order", verbose_name=_("Commande"), on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", verbose_name=_("Produit"), on_delete=models.SET_NULL,null=True, blank=True)
+    quantity = models.IntegerField(_("Quantite"))
+    price =models.FloatField(_("Prix"))
+     
+
+    def __str__(self):
+        return str(self.order)
+
+    class Meta:
+        verbose_name = 'OrderDetail'
+        verbose_name_plural = 'OrderDetails' 
