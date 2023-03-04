f023fd525db8475e2f7e358cd1e4762dd64f998a
rabi3e <65637491+rabi3e@users.noreply.github.com>
2023-03-04 23:48:59 +0100

    add order end cart in admin panel

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,46 @@ from product.models import Product
 from django.contrib.auth.models import User
 from utils.generateur import generate_code
 
-# Create your models here.
+
+
+
+
+# Create Cart models
+CART_STATUS = (
+    ('E', 'En cours'),
+    ('V', 'Validé'),
+
+)
+
+
+class Cart(models.Model):
+    user= models.ForeignKey(User, verbose_name=_("Client"), related_name='user_cart' ,on_delete=models.SET_NULL,null=True,blank=True)
+    status = models.CharField(_("Status"), max_length=1, choices=CART_STATUS)
+    
+
+    def __str__(self):
+        return str(self.code)
+        
+    class Meta:
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Casts'
+    
+
+class CartDetail(models.Model):
+    cart= models.ForeignKey(Cart, verbose_name=_("Commande"), related_name='cart_details' ,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name=_("Produit"), related_name='cart_product',on_delete=models.SET_NULL,null=True, blank=True)
+    quantity = models.IntegerField(_("Quantite"))
+    price =models.FloatField(_("Prix"))
+    total = models.FloatField(_("Total"))
+     
+
+    def __str__(self):
+        return str(self.order)
+
+    class Meta:
+        verbose_name = 'CartDetail'
+        verbose_name_plural = 'CartDetails' 
+# Create Order models
 ORDER_STATUS = (
     ('R', 'Reçue'),
     ('T', 'Traitée'),
@@ -14,7 +53,7 @@ ORDER_STATUS = (
 
 
 class Order(models.Model):
-    user= models.ForeignKey(User, verbose_name=_("Client"), on_delete=models.SET_NULL,null=True,blank=True)
+    user= models.ForeignKey(User, verbose_name=_("Client"),related_name='user_order' ,on_delete=models.SET_NULL,null=True,blank=True)
     code = models.CharField(_("Code"), max_length=50, default=generate_code)
     status = models.CharField(_("Status"), max_length=1, choices=ORDER_STATUS)
     order_time = models.DateTimeField(_("Heure de Commande"), auto_now=True)
@@ -22,7 +61,7 @@ class Order(models.Model):
 
 
     def __str__(self):
-        self.code
+        return self.code
 
     class Meta:
         verbose_name = 'Order'
@@ -30,8 +69,8 @@ class Order(models.Model):
     
 
 class OrderDetail(models.Model):
-    ordre= models.ForeignKey(Order, verbose_name=_("Commande"), on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name=_("Produit"), on_delete=models.SET_NULL,null=True, blank=True)
+    ordre= models.ForeignKey(Order, verbose_name=_("Commande"),related_name='order_detail' ,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name=_("Produit"), related_name='order_porduct' ,on_delete=models.SET_NULL,null=True, blank=True)
     quantity = models.IntegerField(_("Quantite"))
     price =models.FloatField(_("Prix"))
      
