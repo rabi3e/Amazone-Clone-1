dedd77d213afdd39fa024d978c77d3bc1df42367
rabi3e <65637491+rabi3e@users.noreply.github.com>
2023-03-14 22:49:37 +0100

    add product to cart in product_list.html, show cart in base.html

--- orders/get_card.py
+++ orders/get_card.py
@@ -4,6 +4,8 @@ from .models import Cart, CartDetail
 def panier(request):
     if request.user.is_authenticated:
         
-        cart = Cart.objects.get_or_create(user= request.user , status = 'E')
-        #Cart_detail = CartDetail.objects.filter()
-        return {'cart':cart}
+        cart, created = Cart.objects.get_or_create(user= request.user , status = 'E')
+        Cart_detail = CartDetail.objects.filter(cart = cart)
+        return {'cart':cart , 'cart_detail':Cart_detail}
+    else :
+        return{}
